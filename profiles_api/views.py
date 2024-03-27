61e674688cc1c0c30124a8a824e459f0ff8784e9
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-03-27 17:14:28 +0100

    views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+class HelloApiView(APIVIew):
+    """Test API View"""
 
-class HelloApiView(APIView):
-    
+
+
